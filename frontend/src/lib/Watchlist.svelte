0c4e6f1cf7025d1f1a60ab8667d8df43a106ae6d
chris-avens <chrisianavendano@gmail.com>
2024-09-25 23:21:08 +0800

    added async functionalities

--- frontend/src/lib/Watchlist.svelte
+++ frontend/src/lib/Watchlist.svelte
@@ -1,8 +1,19 @@
 <script>
+  import { getIndexYahoo, round } from "../utils";
+
   let watchlist = [
     { symbol: "AAPL", name: "Apple Inc.", price: 150, change: "-0.5%" },
     { symbol: "GOOGL", name: "Alphabet Inc.", price: 2800, change: "+1.2%" },
+    { symbol: "BTC", name: "Bitcoin", price: 2800, change: "+1.2%" },
   ];
+  async function fillInData() {
+    for await (const index of watchlist) {
+      index.data = await getIndexYahoo(index.symbol);
+    }
+  }
+  //   watchlist.forEach(index => {
+  //     index.data = await
+  //   })
 </script>
 
 <section class="watchlist">
@@ -12,21 +23,35 @@
       <tr>
         <th>Symbol</th>
         <th>Name</th>
-        <th>Price</th>
+        <th>Open</th>
         <th>Change</th>
+        <th>Close</th>
+        <th>High</th>
+        <th>Low</th>
       </tr>
     </thead>
     <tbody>
-      {#each watchlist as stock}
-        <tr>
-          <td>{stock.symbol}</td>
-          <td>{stock.name}</td>
-          <td>{stock.price}</td>
-          <td class={stock.change.startsWith("+") ? "positive" : "negative"}
-            >{stock.change}</td
-          >
-        </tr>
-      {/each}
+      {#await fillInData()}
+        <tr><td colspan="7"><div>...loading</div></td></tr>
+      {:then response}
+        {#each watchlist as stock}
+          <tr>
+            <td>{stock.symbol}</td>
+            <td>{stock.name}</td>
+            <td>{round(stock.data.open, 4)}</td>
+            <td
+              class={stock.data.close > stock.data.open
+                ? "positive"
+                : "negative"}
+            >
+              {round(+stock.data.close - +stock.data.open, 4)}
+            </td>
+            <td>{round(stock.data.close, 4)}</td>
+            <td>{round(stock.data.high, 4)}</td>
+            <td>{round(stock.data.low, 4)}</td>
+          </tr>
+        {/each}
+      {/await}
     </tbody>
   </table>
 </section>
@@ -56,7 +81,17 @@
     color: green;
   }
 
+  .positive::before {
+    content: "+";
+  }
+
   .negative {
     color: red;
+  }
+
+  table div {
+    color: rgb(91, 219, 166);
+    text-align: center;
+    width: 100%;
   }
 </style>
---
+++ frontend/src/utils.js
@@ -0,0 +1,31 @@
+
+
+export const round = (num, digits) => {
+    const mult = Math.pow(10, digits);
+    return Math.round(mult * num) / mult;
+};
+
+export async function getIndexYahoo(index) {
+    const response = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://query1.finance.yahoo.com/v8/finance/chart/${index}`,
+    );
+
+    if(response.ok) {
+        const data = await response.json();
+        console.log(data.chart.result[0]);
+        // console.log({...data.chart.result[0].indicators.quote[0], timestamp: data.chart.result[0].timestamp});
+        // console.log(Object.fromEntries(
+        //     Object.entries(data.chart.result[0].indicators.quote[0])
+        //         .map(([key, arr]) => [key, arr.at(-1)])
+        // ));
+        return {
+            ...Object.fromEntries(
+                Object.entries(data.chart.result[0].indicators.quote[0])
+                    .map(([key, arr]) => [key, arr.at(-1)])
+            ),
+            timestamp: data.chart.result[0].timestamp.at(-1)
+        };
+    } else {
+        throw new Error('Request failed');
+    }
+}
